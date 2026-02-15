aeeefc9abc2cf5e1b162bff49f4b0b1f5bb9ed0b
Daniel Zelikov <dzelikov@emich.edu>
2026-02-15 03:11:10 -0500

    Programmer 2 did this

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -1,6 +1,8 @@
+package eclipseSoloProjA;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Howdy github!");
         System.out.println("Second line");
+        System.out.println("Programmer 2 did this");
     }
 }
