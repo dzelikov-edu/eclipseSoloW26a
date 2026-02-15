9b90ae27b593ee4c4e9344a7c6cc2bc7712f2a4c
Daniel Zelikov <dzelikov@emich.edu>
2026-02-15 03:34:10 -0500

    Programmer 1 update

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Howdy github!");
         System.out.println("Second line");
-        System.out.println("Programmer 2 did this");
+        System.out.println("Programmer 2 did this Programmer 1 did this");
     }
 }
